aa0e640fe105feaefac9cd4be8a71885d2655507
Martin Leucht <martin.leucht@gmail.com>
2015-03-19 20:00:07 +0100

    Revert "Revert "added nhibernate and sql connection""
    This reverts commit 075f1b8e87908a827ea870f0f7f5f04c2c5dbbef.

---
+++ Surrogat/Surrogat/Models/TestBE.cs
@@ -0,0 +1,28 @@
+﻿namespace Surrogat.Models
+{
+    using System;
+    using System.Security.Cryptography.X509Certificates;
+
+    using FluentNHibernate.Conventions.Helpers;
+    using FluentNHibernate.Mapping;
+
+    public class TestBE
+    {
+        public TestBE()
+        {
+            Id = Guid.NewGuid();
+        }
+        public virtual Guid Id { get; set; }
+        public virtual string TestField { get; set; }
+    }
+
+    public class TestMap : ClassMap<TestBE>
+    {
+        public TestMap()
+        {
+            this.Table("SG_Test");
+            this.Id(x => x.Id).Column("Id").GeneratedBy.Assigned();
+            Map(x => x.TestField).Column("testfield");
+        }
+    }
+}
---
+++ Surrogat/Surrogat/Models/TestRepository.cs
@@ -0,0 +1,48 @@
+﻿namespace Surrogat.Models
+{
+    using System;
+
+    using FluentNHibernate.Cfg;
+    using FluentNHibernate.Cfg.Db;
+
+    using NHibernate;
+
+    public class BaseRepo : IDisposable
+    {
+        public ISession Session;
+
+        public BaseRepo()
+        {
+            var sessionFactory = CreateSessionFactory();
+            this.Session = sessionFactory.OpenSession();
+        }
+
+        private static ISessionFactory CreateSessionFactory()
+        {
+            return Fluently.Configure()
+              .Database(MsSqlConfiguration.MsSql2008
+              .ConnectionString("Server=tcp:s9vjvqrycz.database.windows.net,1433;Database=fro;User ID=flo@s9vjvqrycz;Password=Fr0r1der-;Trusted_Connection=False;Encrypt=True;Connection Timeout=30;"))
+              .Mappings(m =>
+                m.FluentMappings.AddFromAssemblyOf<TestBE>())
+              .BuildSessionFactory();
+        }
+
+        public void Dispose()
+        {
+            Session.Dispose();
+        }
+    }
+
+    public class TestRepository : BaseRepo
+    {
+        public Guid InsertEntry(TestBE test)
+        {
+            using (var transaction = this.Session.BeginTransaction())
+            {
+                Session.Save(test);
+                transaction.Commit();
+                return test.Id;
+            }
+        }
+    }
+}
