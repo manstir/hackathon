4050d167607f99f73f95a6148b34756492608239
search <Serge Glaeser>
2015-03-19 21:31:55 +0100

    qr lib added, implemented two qr code examples

---
+++ Surrogat/Surrogat/Controllers/QRCodeGeneratorController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Web.Mvc;
+using Gma.QrCodeNet.Encoding;
+using Gma.QrCodeNet.Encoding.Windows.Render;
+
+namespace Surrogat.Controllers
+{
+    public class QRCodeGeneratorController : Controller
+    {
+        public ActionResult BarcodeImage(String barcodeText)
+        {
+            // generating a barcode here. Code is taken from QrCode.Net library
+            QrEncoder qrEncoder = new QrEncoder(ErrorCorrectionLevel.L);
+            QrCode qrCode = new QrCode();
+            qrEncoder.TryEncode(barcodeText, out qrCode);
+            GraphicsRenderer renderer = new GraphicsRenderer(new FixedModuleSize(5, QuietZoneModules.Four), Brushes.Black, Brushes.White);
+
+            Stream memoryStream = new MemoryStream();
+            renderer.WriteToStream(qrCode.Matrix, ImageFormat.Png, memoryStream);
+
+            // very important to reset memory stream to a starting position, otherwise you would get 0 bytes returned
+            memoryStream.Position = 0;
+
+            var resultStream = new FileStreamResult(memoryStream, "image/png");
+            resultStream.FileDownloadName = String.Format("{0}.png", barcodeText);
+
+            return resultStream;
+        }
+    }
+}
