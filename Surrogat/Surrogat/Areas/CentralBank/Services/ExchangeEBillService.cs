3c277c9a2fb85dff28b484fe276aec6d51bccc85
manstir <florian@scheiwiller.ch>
2015-03-20 17:34:17 +0100

    added interface for IExchangeEBillService

---
+++ Surrogat/Surrogat/Areas/CentralBank/Services/ExchangeEBillService.cs
@@ -0,0 +1,14 @@
+﻿namespace Surrogat.Areas.CentralBank.Services
+{
+    using System.Collections.Generic;
+
+    using Surrogat.Shared;
+
+    public class ExchangeEBillService : IExchangeEBillService
+    {
+        public List<BillDto> Exchange(IEnumerator<BillDto> bills, decimal targetAmount)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Surrogat/Surrogat/Areas/CentralBank/Services/IExchangeEBillService.cs
@@ -0,0 +1,11 @@
+﻿namespace Surrogat.Areas.CentralBank.Services
+{
+    using System.Collections.Generic;
+
+    using Surrogat.Shared;
+
+    public interface IExchangeEBillService
+    {
+        List<BillDto> Exchange(IEnumerator<BillDto> bills, decimal targetAmount);
+    }
+}
