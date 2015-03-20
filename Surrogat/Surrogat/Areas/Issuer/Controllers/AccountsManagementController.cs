a2eda4d735a550f97f6ff7a6f857e237cebca9e2
DioF <martin.leucht@gmail.com>
2015-03-20 15:44:37 +0100

    added area Issuer

---
+++ Surrogat/Surrogat/Areas/Issuer/Controllers/AccountsManagementController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Surrogat.Areas.Issuer.Controllers
+{
+    public class AccountsManagementController : Controller
+    {
+        // GET: Issuer/AccountsManagement
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Surrogat/Surrogat/Areas/Issuer/IssuerAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace Surrogat.Areas.Issuer
+{
+    public class IssuerAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Issuer";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Issuer_default",
+                "Issuer/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
