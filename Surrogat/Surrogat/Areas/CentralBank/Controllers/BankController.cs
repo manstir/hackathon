e48934158c831536f4a73656dce8085494ed813f
manstir <florian@scheiwiller.ch>
2015-03-20 18:19:34 +0100

    added action to test eBillExchange

--- Surrogat/Surrogat/Areas/CentralBank/Controllers/BankController.cs
+++ Surrogat/Surrogat/Areas/CentralBank/Controllers/BankController.cs
@@ -1,11 +1,14 @@
 ﻿namespace Surrogat.Areas.CentralBank.Controllers
 {
+    using System;
+    using System.Linq;
     using System.Web.Mvc;
 
     using FluentNHibernate.Conventions.AcceptanceCriteria;
 
     using Surrogat.Areas.CentralBank.Repository;
     using Surrogat.Areas.CentralBank.ViewModels;
+    using Surrogat.Shared;
 
     public class BankController  : Controller
     {
@@ -23,6 +26,14 @@
         {
             var srv = new Services.OrderEBillService();
             srv.OrderBill(1, 200);
+            return RedirectToAction("index");
+        }
+
+        public ActionResult ExchangeTest()
+        {
+            var srv = new Services.ExchangeEBillService();
+            var x = srv.Exchange(new[] {new BillDto{Token = new Guid("29be1e67-a008-4a53-b248-c820d746df86")}, new BillDto{Token = new Guid("29bf48a3-e2f1-49b7-8787-68d693fe7c87") }}, 45);
+            x.ToList();
             return RedirectToAction("index");
         }
 
