6efbf862c48ab16c9b105162bb4ecb4c40494314
DioF <martin.leucht@gmail.com>
2015-03-20 20:02:32 +0100

    added BillBE

---
+++ Surrogat/Surrogat/Areas/Issuer/Models/BillBE.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using FluentNHibernate.Mapping;
+
+namespace Surrogat.Areas.Issuer.Models
+{
+    public class BillMap : ClassMap<BillBE>
+    {
+        public BillMap()
+        {
+            Table("SG_IS_Bill");
+            Id(x => x.Id).Column("Id");
+            Map(x => x.Token).Column("Token");
+            Map(x => x.Amount).Column("Amount");
+            Map(x => x.BoughtOn).Column("BoughtOn");
+        }
+    }
+
+    public class BillBE
+    {
+        public virtual int Id { get; set; }
+        public virtual string Token { get; set; }
+        public virtual decimal Amount { get; set; }
+        public virtual DateTime BoughtOn { get; set; }}
+}
--- Surrogat/Surrogat/Areas/Issuer/ViewModels/ShowAccountsViewModel.cs
+++ Surrogat/Surrogat/Areas/Issuer/ViewModels/ShowAccountsViewModel.cs
@@ -6,6 +6,6 @@ namespace Surrogat.Areas.Issuer.ViewModels
     public class ShowAccountsViewModel
     {
         public IReadOnlyCollection<AccountBE> Accounts { get; set; }
-        public IReadOnlyCollection<AccountBE> Bills { get; set; }
+        public IReadOnlyCollection<BillBE> Bills { get; set; }
     }
 }
