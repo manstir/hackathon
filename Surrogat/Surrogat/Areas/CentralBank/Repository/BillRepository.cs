7462d2ca0a7edcf29f63a064d304fc7077854b57
manstir <florian@scheiwiller.ch>
2015-03-20 18:27:01 +0100

    fix repo merge

--- Surrogat/Surrogat/Areas/CentralBank/Repository/BillRepository.cs
+++ Surrogat/Surrogat/Areas/CentralBank/Repository/BillRepository.cs
@@ -1,5 +1,6 @@
 ﻿namespace Surrogat.Areas.CentralBank.Repository
 {
+    using System;
     using System.Collections.Generic;
     using System.Linq;
 
@@ -10,14 +11,47 @@
 
     public class BillRepository : BaseRepo
     {
+        public BillBE CreateBill(decimal amount)
+        {
+            using (var transaction = Session.BeginTransaction())
+            {
+                var serial = (int)Session.Save(new BillBE { Amount = amount, IssuedDate = DateTime.Now, Token = Guid.NewGuid(), });
+                var bill = this.LoadBillInternal(serial);
+                transaction.Commit();
+                return bill;
+            }
+        }
+
         public IReadOnlyCollection<BillBE> GetAllBills()
         {
-                using (var transaction = Session.BeginTransaction())
-                {
-                    var bills =  Session.Query<BillBE>().Where(b => b.Serial < 100).ToList();
-                    transaction.Commit();
-                    return bills;
-                }
-        }    
+            using (var transaction = Session.BeginTransaction())
+            {
+                var bills = Session.Query<BillBE>().Where(b => b.Serial < 100).ToList();
+                transaction.Commit();
+                return bills;
+            }
+        }
+
+        public decimal CashBill(Guid token)
+        {
+            using (var transaction = Session.BeginTransaction())
+            {
+                var bill = this.LoadBillByTokenInternal(token);
+                bill.CashedDate = DateTime.Now;
+                var value = bill.Amount;
+                transaction.Commit();
+                return value;
+            }
+        }
+
+        private BillBE LoadBillInternal(int serial)
+        {
+            return Session.Query<BillBE>().FirstOrDefault(b => b.Serial == serial);
+        }
+
+        private BillBE LoadBillByTokenInternal(Guid token)
+        {
+            return Session.Query<BillBE>().FirstOrDefault(b => b.Token == token);
+        }
     }
 }
