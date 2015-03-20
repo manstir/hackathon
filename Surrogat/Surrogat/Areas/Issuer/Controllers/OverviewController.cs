3e09eb63be2d24f64ab3d3eb83b68aaf7a4f25ed
DioF <martin.leucht@gmail.com>
2015-03-20 19:44:59 +0100

    replaced AccountsManagementController with OverviewController

---
+++ Surrogat/Surrogat/Areas/Issuer/Controllers/OverviewController.cs
@@ -0,0 +1,26 @@
+﻿using System.Web.Mvc;
+using Surrogat.Areas.Issuer.ViewModels;
+
+namespace Surrogat.Areas.Issuer.Controllers
+{
+    public class OverviewController : Controller
+    {
+        // GET: Issuer/AccountsManagement
+        public ActionResult Index()
+        {
+            return RedirectToAction("Overview");
+        }
+
+        public ActionResult Overview()
+        {
+            var repository = new AccountsRepository();
+            var accounts = repository.GetAccounts();
+
+            var model = new ShowAccountsViewModel()
+            {
+                Accounts = accounts
+            };
+            return View(model);
+        }
+    }
+}
