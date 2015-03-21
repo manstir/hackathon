44f5865b4abb98a27baabaf9096a1c3992bb629e
search <Serge Glaeser>
2015-03-21 10:25:44 +0100

    acquirer area added

---
+++ Surrogat/Surrogat/Areas/Acquirer/AcquirerAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace Surrogat.Areas.Acquirer
+{
+    public class AcquirerAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Acquirer";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Acquirer_default",
+                "Acquirer/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
