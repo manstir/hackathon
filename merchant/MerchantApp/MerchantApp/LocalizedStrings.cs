268b4ebb4d70957c8a7d0bac95d81875b1b3150f
manstir <florian@scheiwiller.ch>
2015-03-21 09:46:49 +0100

    added merchant phone app

---
+++ merchant/MerchantApp/MerchantApp/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using MerchantApp.Resources;
+
+namespace MerchantApp
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
---
+++ merchant/MerchantApp/MerchantApp/MainPage.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using MerchantApp.Resources;
+
+namespace MerchantApp
+{
+    using System.IO;
+    using System.Windows.Media.Imaging;
+
+    using Microsoft.Phone.Tasks;
+
+    using ZXing;
+
+    public partial class MainPage : PhoneApplicationPage
+    {
+        PhotoChooserTask photoChooserTask;
+
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+
+            photoChooserTask = new PhotoChooserTask();
+            photoChooserTask.Completed += new EventHandler<PhotoResult>(photoChooserTask_Completed);
+
+            // Sample code to localize the ApplicationBar
+            //BuildLocalizedApplicationBar();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            photoChooserTask.Show();
+           
+        }
+
+        void photoChooserTask_Completed(object sender, PhotoResult e)
+        {
+            if (e.TaskResult == TaskResult.OK)
+            {
+                // create a barcode reader instance
+                IBarcodeReader reader = new BarcodeReader();
+                // load a bitmap
+                var image = new BitmapImage();
+                image.SetSource(e.ChosenPhoto);
+                var barcode = new WriteableBitmap(image);
+                // detect and decode the barcode inside the bitmap
+                var result = reader.Decode(barcode);
+                // do something with the result
+                if (result != null)
+                {
+                    MessageBox.Show(result.Text.ToString());
+                }
+
+
+                //Code to display the photo on the page in an image control named myImage.
+                //System.Windows.Media.Imaging.BitmapImage bmp = new System.Windows.Media.Imaging.BitmapImage();
+                //bmp.SetSource(e.ChosenPhoto);
+                //myImage.Source = bmp;
+            }
+        }
+
+        // Sample code for building a localized ApplicationBar
+        //private void BuildLocalizedApplicationBar()
+        //{
+        //    // Set the page's ApplicationBar to a new instance of ApplicationBar.
+        //    ApplicationBar = new ApplicationBar();
+
+        //    // Create a new button and set the text value to the localized string from AppResources.
+        //    ApplicationBarIconButton appBarButton = new ApplicationBarIconButton(new Uri("/Assets/AppBar/appbar.add.rest.png", UriKind.Relative));
+        //    appBarButton.Text = AppResources.AppBarButtonText;
+        //    ApplicationBar.Buttons.Add(appBarButton);
+
+        //    // Create a new menu item with the localized string from AppResources.
+        //    ApplicationBarMenuItem appBarMenuItem = new ApplicationBarMenuItem(AppResources.AppBarMenuItemText);
+        //    ApplicationBar.MenuItems.Add(appBarMenuItem);
+        //}
+    }
+}
